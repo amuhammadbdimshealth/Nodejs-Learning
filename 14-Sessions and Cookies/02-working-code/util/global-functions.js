981473792c1e684515cd49ce9ece2d5730f4325b
arif <amuhammadbdimshealth@gmail.com>
2020-04-12 11:12:10 +0600

    handle cases when no cookies exists in the browser

--- 14-Sessions and Cookies/02-working-code/util/global-functions.js
+++ 14-Sessions and Cookies/02-working-code/util/global-functions.js
@@ -1,17 +1,25 @@
-exports.getCookie = req => {
-  const loggedInCookieExists = req
-      .get("Cookie")
-      .split(";")    
-      .find(cookie => {
-        console.log('cookie: ', cookie)
-        if(cookie) return cookie.trim().split("=")[0] == 'loggedIn'                
-      })
-  console.log('loggedInCookieExists',loggedInCookieExists);
-  const loggedInCookie = loggedInCookieExists ? 
-    req
-      .get("Cookie")
-      .split(";")[1]
-      .trim()
-      .split("=")[1] == 'true' : 'false';
-    return loggedInCookie;
+exports.getCookie = (req) => {
+  const loggedInCookieExists = req.get("Cookie")
+    ? req
+        .get("Cookie")
+        .split(";")
+        .find((cookie) => {
+          console.log("cookie: ", cookie);
+          if (cookie) return cookie.trim().split("=")[0] == "loggedIn";
+        })
+    : false;
+  console.log("10-loggedInCookieExists", loggedInCookieExists);
+  const loggedInCookie = loggedInCookieExists
+    ? req
+        .get("Cookie")
+        .split(";")
+        .find((cookie) => {
+          console.log("cookie: ", cookie);
+          if (cookie) return cookie.trim().split("=")[0] == "loggedIn";
+        })
+    : null;
+  const loggedInCookieValue = loggedInCookieExists
+    ? loggedInCookie.trim().split("=")[1] == "true"
+    : false;
+  return loggedInCookieValue;
 };
