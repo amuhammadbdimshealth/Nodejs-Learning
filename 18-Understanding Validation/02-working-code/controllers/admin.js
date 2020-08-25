94086b1fb5f0c98dddaf7ce9f812703feb06013c
Arif Mohammad Sultan <amuhammadbdimshealth@gmail.com>
2020-08-25 16:41:32 +0600

    S18 - Admin product add and edit fields validation

--- 18-Understanding Validation/02-working-code/controllers/admin.js
+++ 18-Understanding Validation/02-working-code/controllers/admin.js
@@ -2,17 +2,19 @@ const mongodb = require("mongodb");
 const ObjectId = mongodb.ObjectId;
 const Product = require("../models/product");
 const product = require("../models/product");
+const { validationResult } = require("express-validator");
 
 // MIDDLEWARES - FOR HTTP GET REQUESTS
 exports.getAddProduct = (req, res, next) => {
   if (!req.session.isLoggedIn) {
-    console.log("A A NOT LOGGED IN!");
     return res.redirect("/login");
   }
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    errorMessages: [],
+    hasError: false,
     // isAuthenticated: req.session.isLoggedIn
   });
 };
@@ -34,6 +36,8 @@ exports.getEditProduct = (req, res, next) => {
         editing: editMode,
         product: product,
         userID: product.userid.toString(),
+        errorMessages: [],
+        hasError: false,
         // isAuthenticated: req.session.isLoggedIn
       });
     })
@@ -64,8 +68,28 @@ exports.getProducts = (req, res, next) => {
 
 // MIDDLEWARES - FOR HTTP POST REQUESTS
 exports.postAddProduct = (req, res, next) => {
-  //MONGO
-  const { title, imageUrl, description, price } = req.body; //object destructuring
+  const errors = validationResult(req);
+  const { title, imageUrl, description, price } = req.body;
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(402).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      errorMessages: errors.array(),
+      infoMessages: [],
+      hasError: true,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description,
+      },
+    });
+    // return res.status(400).json({ errors: errors.array() });
+  }
+
   const product = new Product({
     title: title,
     imageUrl: imageUrl,
@@ -87,6 +111,26 @@ exports.postAddProduct = (req, res, next) => {
 exports.postEditProduct = (req, res, next) => {
   // get the updated product from the req
   const { productId, title, price, imageUrl, description } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(402).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      errorMessages: errors.array(),
+      infoMessages: [],
+      hasError: true,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description,
+        _id: productId,
+      },
+    });
+    // return res.status(400).json({ errors: errors.array() });
+  }
   // check if logged in user is the owner of this product before editing
   const loggedUserId = req.user._id.toString();
   Product.findOne({ _id: productId, userid: loggedUserId }).then((product) => {
--- 18-Understanding Validation/02-working-code/routes/admin.js
+++ 18-Understanding Validation/02-working-code/routes/admin.js
@@ -1,27 +1,38 @@
-const path = require('path');
-const express = require('express');
-const adminController = require('../controllers/admin');
+const path = require("path");
+const express = require("express");
+const adminController = require("../controllers/admin");
 const router = express.Router();
-const isAuth = require('../middleware/is-auth');
-const sendMail = require('../controllers/sendmail');
-const sendMailSendgrid = require('../controllers/sendmail-sendgrid');
+const isAuth = require("../middleware/is-auth");
+const sendMail = require("../controllers/sendmail");
+const sendMailSendgrid = require("../controllers/sendmail-sendgrid");
+const { addProductValidators } = require("../validators/admin");
 
 // // /admin/add-product => GET
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get("/add-product", isAuth, adminController.getAddProduct);
 
 // // /admin/products => GET
-router.get('/products', isAuth, adminController.getProducts);
+router.get("/products", isAuth, adminController.getProducts);
 
 // // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  isAuth,
+  addProductValidators,
+  adminController.postAddProduct
+);
 
 // // /admin/edit-product => GET
-router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
 // // /admin/edit-product => POST
-router.post('/edit-product', isAuth, adminController.postEditProduct);
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
-router.get('/sendmail', sendMail.sendEmailTest)
-router.get('/sendmail-sendgrid', sendMailSendgrid.sendEmailTest)
+router.post(
+  "/edit-product",
+  isAuth,
+  addProductValidators,
+  adminController.postEditProduct
+);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.get("/sendmail", sendMail.sendEmailTest);
+router.get("/sendmail-sendgrid", sendMailSendgrid.sendEmailTest);
 
 module.exports = router;
