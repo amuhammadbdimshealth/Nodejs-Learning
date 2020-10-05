ab6a47aa8bc78a5eb5a747715c7813fcab81db8b
Arif Mohammad Sultan <amuhammadbdimshealth@gmail.com>
2020-10-05 15:06:17 +0600

    S19 - Minor fixes

--- 19-Error handling/02-working-code/routes/error.js
+++ 19-Error handling/02-working-code/routes/error.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const errorController = require("../controllers/error");
 
-router.get("404", errorController.get404);
+router.use(errorController.get404);
 router.get("/500", errorController.get500);
 
 module.exports = router;
