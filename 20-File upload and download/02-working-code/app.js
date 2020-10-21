a010223318724fde623d71d9d5a6ecaf94e78848
Arif Mohammad Sultan <amuhammadbdimshealth@gmail.com>
2020-10-21 18:32:27 +0600

    S20 - initial setup file upload

---
+++ 20-File upload and download/02-working-code/app.js
@@ -0,0 +1,131 @@
+// Imports
+const path = require("path");
+
+// const express = require("express");
+const bodyParser = require("body-parser");
+var session = require("express-session"); // Session management // https://github.com/expressjs/session
+var MongoDBStore = require("connect-mongodb-session")(session); // Session management // https://github.com/mongodb-js/connect-mongodb-session#readme
+
+const errorController = require("./controllers/error");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const errorRoutes = require("./routes/error");
+const errorRoutesPlayground = require("./playground/error-route");
+const User = require("./models/user");
+const mongoose = require("mongoose"); // Mongoose
+const csrf = require("csurf"); // CSRF Protection
+const flash = require("connect-flash"); // Flash error messages
+const express = require("express");
+// const multer = require("multer");
+
+//----------------------------------------------------------------------------
+// App
+const app = express();
+
+// DB url
+const MONGODB_URI = `mongodb+srv://amuhammad:24Aug1989@arif-cluster0-r4goo.mongodb.net/shopMongoose?retryWrites=true&w=majority`;
+
+// Session store
+var store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
+store.on("error", function (error) {
+  (error) => {
+    console.log("error MongoDBStore: ", error);
+  };
+});
+
+// Image store
+// const imageStorage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+
+//   // By default, multer removes file extensions so let's add them back
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+
+// CSRF
+const csrfProtection = csrf();
+//----------------------------------------------------------------------------
+// View engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//----------------------------------------------------------------------------
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(flash());
+app.use(csrfProtection);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+// Store the currently logged-in user in the request object from the session
+app.use((req, res, next) => {
+  if (req.session.user) {
+    User.findById(req.session.user._id)
+      .then((user) => {
+        req.user = user; //mongoose model. we do not need to create user object again
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+  } else {
+    next();
+  }
+});
+
+//----------------------------------------------------------------------------
+// Routes
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "playground", "fileupload.html"));
+// });
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.use(errorRoutesPlayground);
+app.use(errorRoutes);
+
+/** Error handling
+ * https://expressjs.com/en/guide/error-handling.html
+ */
+app.use(function (err, req, res, next) {
+  console.log("CAUGHT");
+  res.status(500).render("error/500", {
+    pageTitle: "Error",
+    path: "/500",
+    error: err,
+  });
+  // res.redirect("/500");
+});
+
+// Start server once connected to DB
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
+
+// module.exports = store;
